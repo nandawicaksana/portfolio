922ccf041590b17526c286e1beac38e84d9dcb54
nanda <tech03@ckpramuka.com>
2026-03-31 10:16:55 +0700

    portfolio ready

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  /* config options here */
   images: {
-    unoptimized: true, // Tambahkan ini agar aman saat deploy di Vercel/Static
-  }
+    unoptimized: true, 
+  },
 };
 
 export default nextConfig;
